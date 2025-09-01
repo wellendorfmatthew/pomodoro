2901432c9432d56a9a09fc1754552d24dbc685d5
Matthew Wellendorf <wellendorfmatthew@gmail.com>
2025-09-01 13:53:58 -0400

    Initialized project and added in title to page

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen w-full bg-indigo-950 flex items-center justify-center">
+      <h1 className="font-bold text-4xl text-indigo-50">pomodoro</h1>
+    </div>
+  );
+}
