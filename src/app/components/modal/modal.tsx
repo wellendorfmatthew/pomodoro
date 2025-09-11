8b762246282f3cfb78637db2c553912f5ed7e6b3
Matthew Wellendorf <wellendorfmatthew@gmail.com>
2025-09-11 16:29:52 -0400

    Added ability to open and display modal

--- src/app/components/modal/modal.tsx
+++ src/app/components/modal/modal.tsx
@@ -1,27 +1,30 @@
 import { BiX } from "react-icons/bi"
+import { modes } from "../../../../config/modes"
 
 export default function Modal() {
     return (
-        <div className="rounded-2xl p-6 flex flex-col items-start justify-start gap-4">
-            <div className="flex items-center justify-between">
-                <h1 className="text-primary font-semibold text-4xl">Settings</h1>
-                <BiX size={24} />
-            </div>
-            <hr className="h-1 w-full" />
-            <div className="flex flex-col gap-4 items-start justify-start">
-                <h2>Time (MINUTES)</h2>
-                <div className="flex items-center justify-between">
-                    <div className="flex flex-col gap-2 items-start justify-start">
-                        <label htmlFor="pomodoro">pomodoro</label>
-                        <input type="text" id="pomodoro" value={30} />
-                    </div>
-                    <div className="flex flex-col gap-2 items-start justify-start">
-                        <label htmlFor="short-break">short break</label>
-                        <input type="text" id="short-break" value={5} />
-                    </div>
-                    <div className="flex flex-col gap-2 items-start justify-start">
-                        <label htmlFor="long-break">long break</label>
-                        <input type="text" id="long-break" value={10} />
+        <div className="fixed inset-0 flex flex-col justify-center items-center bg-gray-900/50 z-50">
+            <div className="rounded-2xl p-6 flex flex-col items-start justify-start gap-4 bg-slate-50 text-primary">
+                <div className="flex items-center justify-between w-full">
+                    <h1 className="font-semibold text-4xl">Settings</h1>
+                    <BiX size={24} />
+                </div>
+                <hr className="h-1 w-full" />
+                <div className="flex flex-col gap-4 items-start justify-start">
+                    <h2 className="text-lg font-semibold">Time (MINUTES)</h2>
+                    <div className="flex items-center justify-between">
+                        <div className="flex flex-col gap-2 items-start justify-start">
+                            <label htmlFor="pomodoro" className="text-slate-400 font-semibold text-sm">pomodoro</label>
+                            <input type="text" id="pomodoro" value={30} className="bg-slate-200 font-semibold" />
+                        </div>
+                        <div className="flex flex-col gap-2 items-start justify-start">
+                            <label htmlFor="short-break">short break</label>
+                            <input type="text" id="short-break" value={5} />
+                        </div>
+                        <div className="flex flex-col gap-2 items-start justify-start">
+                            <label htmlFor="long-break">long break</label>
+                            <input type="text" id="long-break" value={10} />
+                        </div>
                     </div>
                 </div>
             </div>
