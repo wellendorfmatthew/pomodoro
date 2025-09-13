c6a56151a615333c9e6f70aede6197784fb05c47
Matthew Wellendorf <wellendorfmatthew@gmail.com>
2025-09-12 20:56:50 -0400

    Added font and color sections to settings modal

--- src/app/components/modal/modal.tsx
+++ src/app/components/modal/modal.tsx
@@ -1,4 +1,4 @@
-import { BiX } from "react-icons/bi"
+import { BiX, BiCheck } from "react-icons/bi"
 import { modes } from "../../../../config/modes"
 
 export default function Modal() {
@@ -15,15 +15,45 @@ export default function Modal() {
                     <div className="flex items-center justify-between">
                         <div className="flex flex-col gap-2 items-start justify-start">
                             <label htmlFor="pomodoro" className="text-slate-400 font-semibold text-sm">pomodoro</label>
-                            <input type="text" id="pomodoro" value={30} className="bg-slate-200 font-semibold" />
+                            <input type="text" id="pomodoro" value={30} className="bg-slate-200 font-semibold rounded-2xl p-4 outline-none" />
                         </div>
                         <div className="flex flex-col gap-2 items-start justify-start">
-                            <label htmlFor="short-break">short break</label>
-                            <input type="text" id="short-break" value={5} />
+                            <label htmlFor="short-break" className="text-slate-400 font-semibold text-sm">short break</label>
+                            <input type="text" id="short-break" value={5} className="bg-slate-200 font-semibold rounded-2xl p-4 outline-none" />
                         </div>
                         <div className="flex flex-col gap-2 items-start justify-start">
-                            <label htmlFor="long-break">long break</label>
-                            <input type="text" id="long-break" value={10} />
+                            <label htmlFor="long-break" className="text-slate-400 font-semibold text-sm">long break</label>
+                            <input type="text" id="long-break" value={10} className="bg-slate-200 font-semibold rounded-2xl p-4 outline-none" />
+                        </div>
+                    </div>
+                </div>
+                <hr className="h-1 w-full" />
+                <div className="flex items-center justify-between w-full">
+                    <h2 className="text-lg font-semibold">Font</h2>
+                    <div className="flex items-center justify-center gap-4">
+                        <div className="rounded-full aspect-square w-12 p-2 flex items-center justify-center bg-slate-200">
+                            <p className="text-lg font-semibold">Aa</p>
+                        </div>
+                        <div className="rounded-full aspect-square w-12 p-2 flex items-center justify-center bg-slate-200">
+                            <p className="text-lg font-semibold">Aa</p>
+                        </div>
+                        <div className="rounded-full aspect-square w-12 p-2 flex items-center justify-center bg-slate-200">
+                            <p className="text-lg font-semibold">Aa</p>
+                        </div>
+                    </div>
+                </div>
+                <hr className="h-1 w-full" />
+                <div className="flex items-center justify-between w-full">
+                    <h2 className="text-lg font-semibold">Color</h2>
+                    <div className="flex items-center justify-center gap-4">
+                        <div className="rounded-full aspect-square w-12 p-2 flex items-center justify-center bg-slate-200">
+                            <BiCheck size={24} />
+                        </div>
+                        <div className="rounded-full aspect-square w-12 p-2 flex items-center justify-center bg-slate-200">
+                            <BiCheck size={24} />
+                        </div>
+                        <div className="rounded-full aspect-square w-12 p-2 flex items-center justify-center bg-slate-200">
+                            <BiCheck size={24} />
                         </div>
                     </div>
                 </div>
